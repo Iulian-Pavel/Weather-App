d5179cb79d738c4c4127504f92726965abbb6edc
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2022-01-04 21:40:53 +0200

    updated to check the coordinates of the user

--- src/components/GetUserLocation/GetUserLocation.jsx
+++ src/components/GetUserLocation/GetUserLocation.jsx
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function GetUserLocation() {
-
-    const [isAvailable, checkIfAvailable] = useState(false);
-    useEffect(() => {
-        if('geolocation' in navigator) {
-            checkIfAvailable(true);
-        } else {
-            checkIfAvailable(false);
-        }
-    })
-    return (
-        <>
-        {isAvailable ? (
-            <p>geolocation is available</p>
-        ) : (
-            <p>geolocation is unavailable</p>
-        )}
-        </>
-    )
+  const [isAvailable, checkIfAvailable] = useState(false);
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      checkIfAvailable(true);
+      navigator.geolocation.getCurrentPosition((position) => {
+          console.log('latitude is:', position.coords.latitude)
+          console.log('longitude is:', position.coords.longitude)
+      })
+    } else {
+      checkIfAvailable(false);
+    }
+  });
+  return (
+    <>
+      {isAvailable ? (
+        <p>geolocation is available</p>
+      ) : (
+        <p>geolocation is unavailable</p>
+      )}
+    </>
+  );
 }
 
 export default GetUserLocation;
