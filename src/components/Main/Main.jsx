b6d5fc3d07b27df4d9947d9f0d34dd7a0b9e59a7
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2022-01-25 19:53:57 +0200

    rewritten apiCall function with axios

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GetUserLocation from "../GetUserLocation/GetUserLocation";
+import axios from "axios";
 import "./main.css";
 
 class Main extends React.Component {
@@ -13,14 +14,11 @@ class Main extends React.Component {
       try {
         let URL =
           "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=a31c2c7beee129eba49814fecf076451";
-        let response = await fetch(URL, {
-          "Content-Type": "application/json",
-        });
-        let weatherResponse = await response.json();
+        const weatherResponse = await axios.get(URL);
         console.log(weatherResponse);
         this.setState({
-          temperature: weatherResponse.main.temp,
-          humidity: weatherResponse.main.humidity,
+          temperature: weatherResponse.data.main.temp,
+          humidity: weatherResponse.data.main.humidity,
         });
       } catch (err) {
         console.log(err);
